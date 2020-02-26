f67445935a0a5bb96efcd211a6089d95ebcae0fc
Johan Manuel Gonzalez <gonzalezjoan@hotmail.com>
2020-02-26 15:35:27 +0100

    Añado el metodo mostrar

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -2,4 +2,12 @@ package sumador;
 
 public class ASumar {
 
+	public String mostrar(String cad) {
+        String numero = cad;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+		return numero;
+	}
+
 }
