c89038239ea5e06fe82debb1a678a59a978a5729
Johan Manuel Gonzalez <gonzalezjoan@hotmail.com>
2020-02-26 19:32:49 +0100

    Refactorizacion de la clase

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -9,25 +9,30 @@ package sumador;
 
 public class ASumar {
 	
-	 public static String sumar(String cadena) {
+	/**
+	 * Convierte a integer la cadena que pasa por parametro y devuelve unacadena con la suma de cada uno de sus valores 
+	 * @param String cadena
+	 * @return String 
+	 */
+	
+	 public static String sumarCadena(String cadena) {
 	        int suma = 0;
 	        String cad = "";
 	        
 	        if(cadena.charAt(0) == '-') return ""; // Si el valor es negativo
-	        if(cadena.length() == 1) return cadena + " = " + cadena;
+	        
+	        if(cadena.length() == 1) return cadena + " = " + cadena; // Si solo se trata de un numero
 
-	        for (int i = 0; i < cadena.length(); i++) {
+	        for (int i = 0; i < cadena.length(); i++) { // Numero mayo a 9
 	            // Cadena de un dígito.
 	        	
 	            String digito = cadena.substring(i, i + 1);
 	            
-	            if(i > 0) {
+	            if(i > 0) { // Contatena cada numero
 	            	cad = cad + " + " + digito;
 	            }else{
 	            	cad = digito;
 	            }
-	            
-	            
 	            
 	            suma = suma + Integer.parseInt(digito);
 	        }
--- test/sumador/TestASumar.java
+++ test/sumador/TestASumar.java
@@ -9,21 +9,21 @@ public class TestASumar {
 	@Test
 	 public void testUnaCifra() {
         ASumar sumi = new ASumar();
-        String resultado = sumi.sumar("7");
+        String resultado = sumi.sumarCadena("7");
         assertEquals("7 = 7",resultado );
     }
 	
 	@Test
 	 public void testValorNegativo() {
        ASumar sumi = new ASumar();
-       String resultado = sumi.sumar("-74");
+       String resultado = sumi.sumarCadena("-74");
        assertEquals("",resultado );
    }
 	
 	@Test
 	 public void testMayorQueNueve() {
       ASumar sumi = new ASumar();
-      String resultado = sumi.sumar("7423");
+      String resultado = sumi.sumarCadena("7423");
       assertEquals("7 + 4 + 2 + 3 = 16",resultado );
   }
 
