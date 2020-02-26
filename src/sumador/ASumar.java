c00911263dcd2da4344d903bc09eac8497ae239e
Johan Manuel Gonzalez <gonzalezjoan@hotmail.com>
2020-02-26 15:46:08 +0100

    Prueba un numero de un solo digito

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -1,7 +1,12 @@
 package sumador;
 
 public class ASumar {
-
+	
+	/**
+	 * Muestra una concatenación de numeros de String "numero = numero"
+	 * @param cad String cadena de numeros
+	 * @return
+	 */
 	public String mostrar(String cad) {
         String numero = cad;
         if (numero.length() == 1) {
